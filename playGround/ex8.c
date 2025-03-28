eafe9ec7e7cca53f05d57671172cafed79e83d23
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-03-28 15:46:52 +0000

    fin for loop

--- playGround/ex8.c
+++ playGround/ex8.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main() {
+    int i;
+    int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int sum = 0;
+    unsigned long long factorial = 1;
 
+    for (i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
+
+    for (i = 0; i < 10; i++) {
+        sum += array[i];
+    }
+
+    for (i = 0; i < 10; i++) {
+        factorial *= array[i];
+    }
+
+    printf("Sum of the array is %d\n", sum);
+    printf("10! is %llu\n", factorial);
 
     return 0;
 }
