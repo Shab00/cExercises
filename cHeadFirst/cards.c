d22870720adee24cddb3001360fd6263aed5bfea
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-06-10 14:11:33 +0100

    fixed conditional in cards

--- cHeadFirst/cards.c
+++ cHeadFirst/cards.c
@@ -13,18 +13,18 @@ int main()
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
     int val = 0;
-    if (card_name[0] == 'K') {
-    val = 10;
-    } else if (card_name[0] == 'Q') {
-        val = 10;
-    } else if (card_name[0] == 'J' ) {
+    if (card_name[0] == 'K' || card_name[0] == 'Q' || card_name[0] == 'J' ) {
         val = 10;
     } else if (card_name[0] == 'A') {
         val = 11;
     } else {
     val = atoi(card_name);
     }
-    printf("The card value is: %i\n", val);
-
+    /*Check if the value is 3 to 6 */
+    if (val >= 3 && val <= 6) {
+        puts("Count has gone up");
+    } else if (val == 10) {
+        puts("Count has gone down");
+    }
     return 0;
 }
