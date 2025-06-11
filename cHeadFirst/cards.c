5edf048557b55908290cfd94be742adb6c28eb4c
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-06-11 18:59:58 +0100

    finish card counting program

--- cHeadFirst/cards.c
+++ cHeadFirst/cards.c
@@ -10,21 +10,38 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    if (card_name[0] == 'K' || card_name[0] == 'Q' || card_name[0] == 'J' ) {
-        val = 10;
-    } else if (card_name[0] == 'A') {
-        val = 11;
-    } else {
-    val = atoi(card_name);
+    int count = 0;
+    while (card_name[0] != 'X') {
+        puts("Enter the card_name: \n");
+        scanf("%2s", card_name);
+        int val = 0;
+
+        switch(card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
+                if (val < 1 || val > 10) {
+                       puts("value out of bounds!\n");
+                       continue;
+                }
+        }
+        if (val > 2 && val < 7) {
+            count ++;
+        } else if (val == 10) {
+            count --;
+        }
+
+        printf("Current count: %i\n", count);
+
     }
-    /*Check if the value is 3 to 6 */
-    if (val >= 3 && val <= 6) {
-        puts("Count has gone up");
-    } else if (val == 10) {
-        puts("Count has gone down");
-    }
-    return 0;
+   return 0;
 }
