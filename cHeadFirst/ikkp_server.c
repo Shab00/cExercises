0213eb316e20911489f20e4e5b8e102a005a96d2
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-11-12 14:39:39 +0000

    imporved ikkp_server

--- cHeadFirst/ikkp_server.c
+++ cHeadFirst/ikkp_server.c
@@ -3,7 +3,9 @@
 #include <strings.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -12,6 +14,7 @@
 int listener_d = 0;
 
 void handle_shutdown(int sig);
+void handle_sigchld(int sig);
 int catch_signal(int sig, void (*handler)(int));
 int open_listener_socket(void);
 void bind_to_port(int socket, int port);
@@ -25,8 +28,20 @@ void handle_shutdown(int sig)
         close(listener_d);
         listener_d = 0;
     }
-    fprintf(stderr, "\nBye!\n");
-    exit(0);
+    const char bye[] = "\nBye!\n";
+    write(STDERR_FILENO, bye, sizeof(bye) - 1);
+    _exit(0);
+}
+
+void handle_sigchld(int sig)
+{
+    (void)sig;
+    while (1) {
+        int status;
+        pid_t pid = waitpid(-1, &status, WNOHANG);
+        if (pid <= 0)
+            break;
+    }
 }
 
 int catch_signal(int sig, void (*handler)(int))
@@ -35,7 +50,7 @@ int catch_signal(int sig, void (*handler)(int))
     memset(&action, 0, sizeof(action));
     action.sa_handler = handler;
     sigemptyset(&action.sa_mask);
-    action.sa_flags = 0;
+    action.sa_flags = SA_RESTART;
     return sigaction(sig, &action, NULL);
 }
 
@@ -114,6 +129,9 @@ int main(int argc, char *argv[])
     if (catch_signal(SIGINT, handle_shutdown) == -1)
         error("Can't set the interrupt handler");
 
+    if (catch_signal(SIGCHLD, handle_sigchld) == -1)
+        error("Can't set SIGCHLD handler");
+
     listener_d = open_listener_socket();
     bind_to_port(listener_d, 30000);
 
@@ -129,32 +147,49 @@ int main(int argc, char *argv[])
     while (1) {
         int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
         if (connect_d == -1) {
+            if (errno == EINTR)
+                continue;
             perror("accept");
             continue;
         }
 
-        if (say(connect_d, "Internet Knock-Knock Protocol Server\r\nVersion 1.0\r\nKnock! Knock!\r\n> ") != -1) {
-            if (read_in(connect_d, buf, sizeof(buf)) == -1) {
-                close(connect_d);
-                continue;
-            }
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            close(connect_d);
+            continue;
+        }
 
-            if (strncasecmp("Who's there?", buf, 12) != 0) {
-                say(connect_d, "You should say 'Who's there?'!\r\n");
-            } else {
-                if (say(connect_d, "Oscar\r\n> ") != -1) {
-                    if (read_in(connect_d, buf, sizeof(buf)) == -1) {
-                        close(connect_d);
-                        continue;
-                    }
+        if (pid == 0) {
+            close(listener_d);
 
-                    if (strncasecmp("Oscar who?", buf, 10) != 0) {
-                        say(connect_d, "You should say 'Oscar who?'!\r\n");
-                    } else {
-                        say(connect_d, "Oscar silly question, you get a silly answer\r\n");
+            if (say(connect_d,
+                    "Internet Knock-Knock Protocol Server\r\nVersion 1.0\r\nKnock! Knock!\r\n> ")
+                != -1) {
+                if (read_in(connect_d, buf, sizeof(buf)) == -1) {
+                    close(connect_d);
+                    _exit(0);
+                }
+
+                if (strncasecmp("Who's there?", buf, 12) != 0) {
+                    say(connect_d, "You should say 'Who's there?'!");
+                } else {
+                    if (say(connect_d, "Oscar\r\n> ") != -1) {
+                        if (read_in(connect_d, buf, sizeof(buf)) == -1) {
+                            close(connect_d);
+                            _exit(0);
+                        }
+
+                        if (strncasecmp("Oscar who?", buf, 10) != 0)
+                            say(connect_d, "You should say 'Oscar who?'!\r\n");
+                        else
+                            say(connect_d, "Oscar silly question, you get a silly answer\r\n");
                     }
                 }
             }
+
+            close(connect_d);
+            _exit(0);
         }
 
         close(connect_d);
