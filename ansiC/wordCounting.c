fcab98fbffea64d010fa5b5339545331c076d035
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-12-18 18:31:31 +0000

    wordcounting

--- ansiC/wordCounting.c
+++ ansiC/wordCounting.c
@@ -7,6 +7,12 @@ int main(){
     while ((c = getchar()) != EOF)
         if (c == '\n')
             ++nl;
-    printf("%d\n", nl);
+        else if (c == '\t')
+            ++t;
+        else if (c == ' ')
+            ++b;
+    printf("The amount of lines: %d\n", nl);
+    printf("The amount of tabs: %d\n", t);
+    printf("The amount of spaces: %d\n", b);
     return 0;
 }
