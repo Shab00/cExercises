ba05908cc69f04576d752da439f5440bfe599f68
Bashaar <47157957+Shab00@users.noreply.github.com>
2026-06-25 20:11:58 +0100

    Corrected code

--- ansiC/chapter6/preprocessor.c
+++ ansiC/chapter6/preprocessor.c
@@ -5,6 +5,7 @@
 
 #define MAXWORD 100
 #define HASHSIZE 101
+#define BUFSIZE 100
 
 struct nlist {
     struct nlist *next;
@@ -13,21 +14,26 @@ struct nlist {
 };
 
 static struct nlist *hashtab[HASHSIZE];
+char buf[BUFSIZE];
+int bufp = 0;
 
 unsigned hash(char *s)
 {
     unsigned hashval;
-    for (hashval = 0; *s != '\0'; s++)
+    for (hashval = 0; *s != '\0'; s++) {
         hashval = *s + 31 * hashval;
+    }
     return hashval % HASHSIZE;
 }
 
 struct nlist *lookup(char *s)
 {
     struct nlist *np;
-    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
-        if (strcmp(s, np->name) == 0)
+    for (np = hashtab[hash(s)]; np != NULL; np = np->next) {
+        if (strcmp(s, np->name) == 0) {
             return np;
+        }
+    }
     return NULL;
 }
 
@@ -37,16 +43,18 @@ struct nlist *install(char *name, char *defn)
     unsigned hashval;
     if ((np = lookup(name)) == NULL) {
         np = (struct nlist *) malloc(sizeof(*np));
-        if (np == NULL || (np->name = strdup(name)) == NULL)
+        if (np == NULL || (np->name = strdup(name)) == NULL) {
             return NULL;
+        }
         hashval = hash(name);
         np->next = hashtab[hashval];
         hashtab[hashval] = np;
     } else {
         free((void *) np->defn);
     }
-    if ((np->defn = strdup(defn)) == NULL)
+    if ((np->defn = strdup(defn)) == NULL) {
         return NULL;
+    }
     return np;
 }
 
@@ -54,140 +62,168 @@ void undef(char *s)
 {
     int h;
     struct nlist *prev, *np;
-
     prev = NULL;
     h = hash(s);
     for (np = hashtab[h]; np != NULL; np = np->next) {
-        if (strcmp(s, np->name) == 0)
+        if (strcmp(s, np->name) == 0) {
             break;
+        }
         prev = np;
     }
     if (np != NULL) {
-        if (prev == NULL)
+        if (prev == NULL) {
             hashtab[h] = np->next;
-        else
+        } else {
             prev->next = np->next;
+        }
         free((void *) np->name);
         free((void *) np->defn);
         free((void *) np);
     }
 }
 
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
-
 int getch(void)
 {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+    if (bufp > 0) {
+        return buf[--bufp];
+    } else {
+        return getchar();
+    }
 }
 
 void ungetch(int c)
 {
-    if (bufp >= BUFSIZE)
+    if (bufp >= BUFSIZE) {
         printf("ungetch: too many characters\n");
-    else
+    } else {
         buf[bufp++] = c;
+    }
 }
 
 void ungets(char s[])
 {
-    int len = strlen(s);
-    while (len > 0)
+    int len;
+    len = strlen(s);
+    while (len > 0) {
         ungetch(s[--len]);
+    }
 }
 
 int getword(char *word, int lim)
 {
-    int c, getch(void);
-    void ungetch(int);
-    char *w = word;
-
-    while (isspace(c = getch()))
-        ;
-    if (c != EOF)
+    int c;
+    char *w;
+    w = word;
+    while ((c = getch()) == ' ' || c == '\t') {
+        putchar(c);
+    }
+    if (c == '\n') {
+        *w = '\n';
+        *(w + 1) = '\0';
+        return '\n';
+    }
+    if (c != EOF) {
         *w++ = c;
+    }
     if (!isalpha(c) && c != '#' && c != '_') {
         *w = '\0';
         return c;
     }
-    for ( ; --lim > 0; w++)
-        if (!isalnum(*w = getch()) && *w != '_') {
+    for ( ; --lim > 0; w++) {
+        *w = getch();
+        if (!isalnum(*w) && *w != '_') {
             ungetch(*w);
             break;
         }
+    }
     *w = '\0';
     return word[0];
 }
 
-void error(int, char *);
-void getdef(void);
-void skipblanks(void);
-
-
-int main()
+void skipblanks(void)
 {
-    char w[MAXWORD];
-    struct nlist *p;
+    int c;
+    while ((c = getch()) == ' ' || c == '\t') {
+        ;
+    }
+    ungetch(c);
+}
 
-    while (getword(w, MAXWORD) != EOF)
-        if (strcmp(w, "#") == 0) 
-            getdef();
-        else if (!isalpha(w[0]) && w[0] != '_')
-            printf("%s", w); 
-        else if ((p = lookup(w)) == NULL)
-            printf("%s", w);
-        else
-            ungets(p->defn);
-    return 0;
+void skipline(void)
+{
+    int c;
+    while ((c = getch()) != '\n' && c != EOF) {
+        ;
+    }
+    if (c == '\n') {
+        putchar('\n');
+    }
+}
+
+void error(int c, char *s)
+{
+    printf("error: %s\n", s);
+    while (c != EOF && c != '\n') {
+        c = getch();
+    }
 }
 
 void getdef(void)
 {
     int c, i;
     char def[MAXWORD], dir[MAXWORD], name[MAXWORD];
-
     skipblanks();
-    if (!isalpha(getword(dir, MAXWORD)))
+    if (!isalpha(getword(dir, MAXWORD))) {
         error(dir[0], "getdef: expecting a directive after #");
-    else if (strcmp(dir, "define") == 0) {
+    } else if (strcmp(dir, "define") == 0) {
         skipblanks();
-        if (!isalpha(getword(name, MAXWORD)) && name[0] != '_')
+        if (!isalpha(getword(name, MAXWORD)) && name[0] != '_') {
             error(name[0], "getdef: non-alpha - name expected");
-        else {
+        } else {
             skipblanks();
-            for (i = 0; i < MAXWORD - 1; i++)
-                if ((def[i] = getch()) == EOF || def[i] == '\n')
+            for (i = 0; i < MAXWORD - 1; i++) {
+                def[i] = getch();
+                if (def[i] == EOF || def[i] == '\n') {
                     break;
+                }
+            }
             def[i] = '\0';
-            if (i <= 0)
+            if (i <= 0) {
                 error('\n', "getdef: incomplete define");
-            else 
+            } else {
                 install(name, def);
+            }
         }
+        putchar('\n');
     } else if (strcmp(dir, "undef") == 0) {
         skipblanks();
-        if (!isalpha(getword(name, MAXWORD)))
+        if (!isalpha(getword(name, MAXWORD))) {
             error(name[0], "getdef: non-alpha in undef");
-        else
+        } else {
             undef(name);
-    } else
+        }
+        putchar('\n');
+    } else {
         error(dir[0], "getdef: expecting a directive after #");
+    }
 }
 
-void error(int c, char *s)
+int main(void)
 {
-    printf("error: %s\n", s);
-    while (c != EOF && c != '\n')
-        c = getch();
-}
-
-void skipblanks(void)
-{
-    int c;
-    while ((c = getch()) == ' ' || c == '\t')
-        ;
-    ungetch(c);
+    char w[MAXWORD];
+    struct nlist *p;
+    while (getword(w, MAXWORD) != EOF) {
+        if (w[0] == '\n') {
+            putchar('\n');
+        } else if (strcmp(w, "#") == 0) {
+            getdef();
+        } else if (!isalpha(w[0]) && w[0] != '_') {
+            printf("%s", w);
+        } else if ((p = lookup(w)) == NULL) {
+            printf("%s", w);
+        } else {
+            ungets(p->defn);
+        }
+    }
+    return 0;
 }
